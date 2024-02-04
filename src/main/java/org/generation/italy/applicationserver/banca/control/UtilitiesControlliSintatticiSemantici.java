aa97b9dc286ade929c529f4ee4abd8aea0b1a2e3
Fanasca Fabrizio <fabri.taz5@live.it>
2024-02-04 12:22:56 +0100

    modificati i controlli semantici e sintattici

--- src/main/java/org/generation/italy/applicationserver/banca/control/UtilitiesControlliSintatticiSemantici.java
+++ src/main/java/org/generation/italy/applicationserver/banca/control/UtilitiesControlliSintatticiSemantici.java
@@ -12,7 +12,7 @@ public class UtilitiesControlliSintatticiSemantici {
 	static
 	boolean checkFormatCodiceFiscale(String codiceFiscale) {
 		
-		Pattern pattern = Pattern.compile("^[a-zA-Z]{6}[0-9]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9]{2}([a-zA-Z]{1}[0-9]{3})[a-zA-Z]{1}$", Pattern.CASE_INSENSITIVE);
+		Pattern pattern = Pattern.compile("^[A-Z]{6}[0-9]{10}$", Pattern.CASE_INSENSITIVE);
 	    Matcher matcher = pattern.matcher(codiceFiscale);
 	    return matcher.find();
 	    
@@ -22,7 +22,7 @@ public class UtilitiesControlliSintatticiSemantici {
 	static
 	boolean checkFormatIban(String iban) {
 		
-		Pattern pattern = Pattern.compile("^(?:(?:IT|SM)\\d{2}[A-Z]\\d{10}[0-9A-Z]{12}|CY\\d{2}[A-Z]\\d{23}|NL\\d{2}[A-Z]{4}\\d{10}|LV\\d{2}[A-Z]{4}\\d{13}|(?:BG|BH|GB|IE)\\d{2}[A-Z]{4}\\d{14}|GI\\d{2}[A-Z]{4}\\d{15}|RO\\d{2}[A-Z]{4}\\d{16}|KW\\d{2}[A-Z]{4}\\d{22}|MT\\d{2}[A-Z]{4}\\d{23}|NO\\d{13}|(?:DK|FI|GL|FO)\\d{16}|MK\\d{17}|(?:AT|EE|KZ|LU|XK)\\d{18}|(?:BA|HR|LI|CH|CR)\\d{19}|(?:GE|DE|LT|ME|RS)\\d{20}|IL\\d{21}|(?:AD|CZ|ES|MD|SA)\\d{22}|PT\\d{23}|(?:BE|IS)\\d{24}|(?:FR|MR|MC)\\d{25}|(?:AL|DO|LB|PL)\\d{26}|(?:AZ|HU)\\d{27}|(?:GR|MU)\\d{28})$", Pattern.CASE_INSENSITIVE);
+		Pattern pattern = Pattern.compile("^[A-Za-z]{4}[0-9]{28}$", Pattern.CASE_INSENSITIVE);
 	    Matcher matcher = pattern.matcher(iban);
 	    return matcher.find();
 	    
@@ -43,5 +43,11 @@ public class UtilitiesControlliSintatticiSemantici {
 		List<String> valuteAmmesseList = Arrays.asList(valuteAmmesseArray);
 		
 		return (valuteAmmesseList.contains(valuta));
+	}
+	
+	public static boolean checkSementicImporto (String importo) {
+		 
+		Float importoFloat = new Float(importo);
+		return (importoFloat>0);
 	}
 }
--- src/main/webapp/form-versamento.jsp
+++ src/main/webapp/form-versamento.jsp
@@ -12,11 +12,13 @@
             <h1>Apri Conto Cliente</h1>
         </center>
         
-        	<form action="/banca/apri-conto-cliente" method="post">
+        	<form action="/WebAppBancaMio/apri-conto-cliente" method="post">
+        	
+        		<p>Per favore, indicare i dati per inserire il versamento!!!.</p>
 	
 				<label for="iban">Iban</label>
 				<br>
-				<input type="text" id="iban" name="iban" minlength="32" maxlength="32" value="" pattern="^(?:(?:IT|SM)\d{2}[A-Z]\d{10}[0-9A-Z]{12}|CY\d{2}[A-Z]\d{23}|NL\d{2}[A-Z]{4}\d{10}|LV\d{2}[A-Z]{4}\d{13}|(?:BG|BH|GB|IE)\d{2}[A-Z]{4}\d{14}|GI\d{2}[A-Z]{4}\d{15}|RO\d{2}[A-Z]{4}\d{16}|KW\d{2}[A-Z]{4}\d{22}|MT\d{2}[A-Z]{4}\d{23}|NO\d{13}|(?:DK|FI|GL|FO)\d{16}|MK\d{17}|(?:AT|EE|KZ|LU|XK)\d{18}|(?:BA|HR|LI|CH|CR)\d{19}|(?:GE|DE|LT|ME|RS)\d{20}|IL\d{21}|(?:AD|CZ|ES|MD|SA)\d{22}|PT\d{23}|(?:BE|IS)\d{24}|(?:FR|MR|MC)\d{25}|(?:AL|DO|LB|PL)\d{26}|(?:AZ|HU)\d{27}|(?:GR|MU)\d{28})$"  />
+				<input type="text" id="iban" name="iban" minlength="32" maxlength="32" value="" pattern="^[A-Z]{2}[a-z]{2}[0-9]{28}$"  />
 		
 				<br>
 				
